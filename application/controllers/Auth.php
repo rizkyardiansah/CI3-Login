f27c0ae1b9b087c8d9e5b8c2c9c0e4df8cf58b43
rizkyardiansah <muhammadrizkyardiansah93@gmail.com>
2021-06-30 15:53:28 +0700

    membuat view login dan registration

---
+++ application/controllers/Auth.php
@@ -0,0 +1,18 @@
+<?php 
+
+class Auth extends CI_Controller {
+    public function index() {
+        $data['title'] = 'Login Page';
+        $this->load->view('templates/auth_header', $data);
+        $this->load->view('auth/login');
+        $this->load->view('templates/auth_footer');
+        
+    }
+    
+    public function registration() {
+        $data['title'] = 'Registration Page';
+        $this->load->view('templates/auth_header', $data);
+        $this->load->view('auth/registration');
+        $this->load->view('templates/auth_footer');
+    }
+}
