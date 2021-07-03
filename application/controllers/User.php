23795ba5402eed6cbd5efa03b04d1dc60144f1dc
rizkyardiansah <muhammadrizkyardiansah93@gmail.com>
2021-07-03 20:44:21 +0700

    membuat page edit profile yang dapat digunakan untuk mengupload foto profile baru

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -34,11 +34,45 @@ class User extends CI_Controller
 
         $data['title'] = 'Edit Profile';
         $data['user'] = $user;
+        
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
 
-        $this->load->view('templates/header', $data);
-        $this->load->view('templates/sidebar', $data);
-        $this->load->view('templates/topbar', $data);
-        $this->load->view('user/edit', $data);
-        $this->load->view('templates/footer');
+        if ($this->form_validation->run()) {
+            $uploadFile = $_FILES['profile_img']['name'];
+            
+            if ($uploadFile) {
+                $config['allowed_types'] = 'gif|jpg|png|jpeg';
+                $config['upload_path'] = './assets/img/profile/';
+                $config['max_size'] = 2048;
+    
+                $this->load->library('upload', $config);
+    
+                if ($this->upload->do_upload('profile_img')) {
+                    $imgName = $this->upload->data('file_name');
+                    $this->db->set('profile_img', $imgName);
+
+                    if ($user['profile_img'] != 'default.jpg') {
+                        unlink(FCPATH . 'assets/img/profile/' . $user['profile_img']);
+                    }
+                } else {
+                    echo $this->upload->display_errors();
+                }
+            }
+            $email = $this->input->post('email');
+            $name = $this->input->post('name');
+
+            $this->db->set('name', $name);
+            $this->db->where('email', $email);
+            $this->db->update('user');
+
+            redirect('user/index');
+
+        } else {
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('user/edit', $data);
+            $this->load->view('templates/footer');
+        }
     }
 }
--- application/views/user/edit.php
+++ application/views/user/edit.php
@@ -2,7 +2,47 @@
 <div class="container-fluid">
 
     <!-- Page Heading -->
-    <h1 class="mb-3"><?= $title; ?></h1>
+    <div class="row">
+        <div class="col-lg-8">
+            <h1 class="mb-3"><?= $title; ?></h1>
+
+            <?= form_open_multipart('user/edit') ?>
+            <div class="form-group row">
+                <label for="email" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="email" name="email" value="<?= $user['email']; ?>" readonly>
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label for="name" class="col-sm-2 col-form-label">Name</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="name" name="name" value="<?= $user['name']; ?>">
+                    <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <div class="col-sm-2">
+                    <label for="picture">Picture</label>
+                </div>
+                <div class="col-sm-3">
+                    <img class="img-thumbnail" src="<?= base_url('assets/img/profile/') . $user['profile_img']; ?>">
+                </div>
+                <div class="col-sm-7">
+                    <div class="input-group mb-3">
+                        <div class="custom-file">
+                            <input type="file" class="custom-file-input" id="profile_img" name="profile_img">
+                            <label class="custom-file-label" for="profile_img">Choose file</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <button class="btn btn-primary float-right" type="submit">Save</button>
+            </form>
+        </div>
+    </div>
 
 </div>
 <!-- /.container-fluid -->
